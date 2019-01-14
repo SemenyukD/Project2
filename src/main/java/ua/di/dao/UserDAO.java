28f15ab02b6849316b3c61a13413ed0ce50aa02b
SemenyukD <semenyuk.c.s@gmail.com>
2019-01-14 02:09:23 +0200

    Update @DataSource

--- src/main/java/ua/di/dao/UserDAO.java
+++ src/main/java/ua/di/dao/UserDAO.java
@@ -1,12 +1,15 @@
 package ua.di.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import ua.di.model.User;
@@ -15,15 +18,50 @@ import ua.di.model.User;
 public class UserDAO {
 
 	@Autowired
-	DataSource dataSource;
-//	private JdbcTemplate jdbcTemplate;
+	private DataSource ds;
 
-	public List<User> getAll() {
-		return ((JdbcTemplate) dataSource).query("select * from users", new BeanPropertyRowMapper<>(User.class));
+	public List<User> getAll() throws SQLException {
+
+		Connection conn = ds.getConnection();
+		PreparedStatement ps = conn.prepareStatement("select * from users");
+
+		List<User> users = new ArrayList<>();
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			User user = new User();
+			user.setName(rs.getString(1));
+			user.setUrl(rs.getString(2));
+			users.add(user);
+		}
+		return users;
+
 	}
 
-	public void add(User user) {
-		((JdbcTemplate) dataSource).update("insert into users values (?,?,?)", user.getName(), user.getUrl());
+	public void add(User user) throws SQLException {
+
+		Connection conn = ds.getConnection();
+
+		PreparedStatement ps = conn.prepareStatement("insert into users values (?,?)");
+		{
+			ps.setString(1, user.getName());
+			ps.setString(2, user.getUrl());
+			ps.execute();
+		}
 
 	}
 }
+
+//
+//	@Autowired
+////	
+//	private JdbcTemplate jdbcTemplate;
+//
+//	public List<User> getAll() {
+//		return jdbcTemplate.query("select * from users", new BeanPropertyRowMapper<>(User.class));
+//	}
+//
+//	public void add(User user) {
+//		jdbcTemplate.update("insert into users values (?,?)", user.getName(), user.getUrl());
+//
+//	}
+//}
