2ccf7264e78e50e5f00875bbbca3db4bea1796ca
SemenyukD <semenyuk.c.s@gmail.com>
2019-01-16 03:03:15 +0200

    Add @GET and @POST

--- src/main/java/ua/di/MainController.java
+++ src/main/java/ua/di/MainController.java
@@ -1,41 +1,39 @@
 package ua.di;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import ua.di.dao.UserDAO;
 import ua.di.model.User;
 
 @Controller
 public class MainController {
 
-	@Autowired
-	private UserDAO userDima;
+	static List<User> users = new ArrayList<>();
 
-//	static List<User> users = new ArrayList<>();
+	@GetMapping("/view/{name}")
 
-//	@GetMapping("/view/{name}")
-//
-//	public String view(@PathVariable("name") String name, Model model) {
-//		model.addAttribute("msg", "Hello " + name);
-//		return "index";
-//	}
-//
-//	@GetMapping("/raw")
-//	@ResponseBody
-//	public String raw() {
-//		return "Raw data";
-//	}
+	public String view(@PathVariable("name") String name, Model model) {
+		model.addAttribute("msg", "Hello " + name);
+		return "index";
+	}
+
+	@GetMapping("/raw")
+	@ResponseBody
+	public String raw() {
+		return "Raw data";
+	}
 
 	@GetMapping("/users")
-	public String getUsers(Model model) throws SQLException {
-		model.addAttribute("users", userDima.getAll());
+	public String getUsers(Model model) {
+		model.addAttribute("users", users);
 		return "/users";
 	}
 
@@ -45,12 +43,12 @@ public class MainController {
 	}
 
 	@PostMapping("/users/new")
-	public String singUp(@ModelAttribute User user) throws SQLException {
-		userDima.add(user);
-		return "redirect:/users";
-
-//	public String singUp(@RequestParam("name") String name, @RequestParam("url") String url) {
-////	users.add(new User(name, url));
+//	public String singUp(@ModelAttribute User user) {
+//		users.add(user);
 //		return "redirect:/users";
+
+	public String singUp(@RequestParam("name") String name, @RequestParam("url") String url) {
+		users.add(new User(name, url));
+		return "redirect:/users";
 	}
 }
